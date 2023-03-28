1bb9ed21ec6b3423545db77a8162ce96215e2e3f
Boris Pavel <boris.pavel@gmx.com>
2023-03-28 13:26:55 +0300

    Add interactivity

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,12 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * 3)];
 }
+
+let playerSelection = '';
+let computerSelection = '';
+let outcome;
+let round = 0;
 
 /* 
 0 - stands for draw
@@ -10,9 +15,6 @@ function getComputerChoice() {
 */
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
     switch (playerSelection) {
         case 'rock':
             switch (computerSelection) {
@@ -46,35 +48,163 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
 
-    for (let i = 0; i < 5; ++i) {
-        //let playerSelection = prompt('Input your choice');
-        let outcome = playRound(playerSelection, getComputerChoice())
-        switch (outcome) {
-            case 0:
-                break;
-            case 1:
-                ++playerScore;
-                break;
-            case 2:
-                ++computerScore;
-                break;
-            default:
-                alert('Something wrong happened!');
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const computerChoice = document.querySelector('#computer-choice');
+
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+
+const btn = document.querySelector('#play-again');
+
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    computerSelection = getComputerChoice();
+    outcome = playRound(playerSelection,computerSelection);
+    computerChoice.maxWidth = '100%';
+    switch(computerSelection)
+    {
+        case 'rock':
+            computerChoice.src = 'img/rock.png';
+            break;
+        case 'paper':
+            computerChoice.src = 'img/paper.png';
+            break;
+        case 'scissors':
+            computerChoice.src = 'img/scissors.png';
+            break;
+    }
+    if(outcome === 1)
+    {
+        let score = +playerScore.textContent;
+        score++;
+        playerScore.textContent = score;
+    } else if (outcome === 2) 
+    {
+        let score = +computerScore.textContent;
+        score++;
+        computerScore.textContent = score;
+    } else{
+        let scorePlayer = +playerScore.textContent;
+        let scoreComputer = +computerScore.textContent;
+        scorePlayer++;
+        scoreComputer++;
+        computerScore.textContent = scoreComputer;
+        playerScore.textContent = scorePlayer;
+    }
+
+    round++;
+    if(checkEndGame())
+        btn.style.display = 'block';
+    else btn.style.display = 'none';;
+})
+
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    computerSelection = getComputerChoice();
+    outcome = playRound(playerSelection,computerSelection);
+    computerChoice.maxWidth = '100%';
+    switch(computerSelection)
+    {
+        case 'rock':
+            computerChoice.src = 'img/rock.png';
+            break;
+        case 'paper':
+            computerChoice.src = 'img/paper.png';
+            break;
+        case 'scissors':
+            computerChoice.src = 'img/scissors.png';
+            break;
+    }
+    if(outcome === 1)
+    {
+        let score = +playerScore.textContent;
+        score++;
+        playerScore.textContent = score;
+    } else if (outcome === 2) 
+    {
+        let score = +computerScore.textContent;
+        score++;
+        computerScore.textContent = score;
+    } else{
+        let scorePlayer = +playerScore.textContent;
+        let scoreComputer = +computerScore.textContent;
+        scorePlayer++;
+        scoreComputer++;
+        computerScore.textContent = scoreComputer;
+        playerScore.textContent = scorePlayer;
+    }
+
+    round++;
+    if(checkEndGame())
+        btn.style.display = 'block';
+    else btn.style.display = 'none';;
+})
+
+scissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    computerSelection = getComputerChoice();
+    outcome = playRound(playerSelection,computerSelection);
+    computerChoice.maxWidth = '100%';
+    switch(computerSelection)
+    {
+        case 'rock':
+            computerChoice.src = 'img/rock.png';
+            break;
+        case 'paper':
+            computerChoice.src = 'img/paper.png';
+            break;
+        case 'scissors':
+            computerChoice.src = 'img/scissors.png';
+            break;
+    }
+    if(outcome === 1)
+    {
+        let score = +playerScore.textContent;
+        score++;
+        playerScore.textContent = score;
+    } else if (outcome === 2) 
+    {
+        let score = +computerScore.textContent;
+        score++;
+        computerScore.textContent = score;
+    } else{
+        let scorePlayer = +playerScore.textContent;
+        let scoreComputer = +computerScore.textContent;
+        scorePlayer++;
+        scoreComputer++;
+        computerScore.textContent = scoreComputer;
+        playerScore.textContent = scorePlayer;
+    }
+
+    round++;
+    if(checkEndGame())
+        btn.style.display = 'block';
+    else btn.style.display = 'none';;
+});
+
+
+function checkEndGame(){
+    if(round > 5)
+        {
+            return true;
         }
-    }
-
-    if (playerScore > computerScore) {
-        console.log('You win!');
-    }
-    else if (playerScore < computerScore) {
-        console.log('You lose!');
-    } else {
-        console.log('Draw');
-    }
+        else return false;
 }
 
-game();
+function resetGame(){
+    computerScore.textContent = '0';
+    playerScore.textContent = '0';
+    btn.style.display = 'none';
+    round = 0;
+    computerChoice.src = 'img/question-mark.png';
+    computerChoice.maxWidth = '70%';
+
+}
+btn.addEventListener('click', ()=>{
+    resetGame();
+});
